dd2e45d56b9e02d8e1cca675fc07510e851efdc0
JFarah <joaquinefarah@gmail.com>
2024-08-14 17:30:54 -0300

    Fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,22 @@
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
+import {unstable_setRequestLocale} from 'next-intl/server'; 
+
+
+const locales = ['es', 'it'];
  
+export function generateStaticParams() {
+  return locales.map((locale) => ({locale}));
+}
+
+
 export default async function LocaleLayout({
   children,
   params: {locale}
 }: {
   children: React.ReactNode;
   params: {locale: string};
-}) {
-  // Providing all messages to the client
-  // side is the easiest way to get started
+}) { unstable_setRequestLocale(locale);
   const messages = await getMessages();
  
   return (
