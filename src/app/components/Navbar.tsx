ef7be431a925638ad875da0430b27312be85c5ef
JFarah <joaquinefarah@gmail.com>
2024-08-16 13:20:11 -0300

    line thickness change

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -151,7 +151,7 @@ const Navbar: React.FC = () => {
       </div>
 
       {/* Línea roja debajo del navbar */}
-      <div className="h-2 bg-gradient-to-r from-green-800 via-white to-red-800"></div>
+      <div className="h-3 bg-gradient-to-r from-green-800 via-white to-red-800"></div>
     </nav>
   );
 };
