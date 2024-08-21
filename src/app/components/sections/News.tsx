a5e46d6f1ffcd2b0d6f95779c1666ec2d0c76c21
JFarah <joaquinefarah@gmail.com>
2024-08-20 22:43:09 -0300

    CSS adjust

--- src/app/components/sections/News.tsx
+++ src/app/components/sections/News.tsx
@@ -28,15 +28,15 @@ const posts = [
   export default function NewsSection() {
     const t = useTranslations('NewsSection');
     return (
-      <div className="bg-white py-24 sm:py-20">
+      <div className="bg-white py-24 sm:py-10">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('header')} </h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
+            <p className="mt-1 text-lg leading-8 text-gray-600">
               {t('header1')}
             </p>
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-2 sm:pt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((post) => (
               <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
                 <div className="flex items-center gap-x-4 text-xs">
