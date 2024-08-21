39956ceba81ac8699a39f911d8567ae6195e295c
JFarah <joaquinefarah@gmail.com>
2024-08-20 21:36:07 -0300

    NewsSection update

--- src/app/components/sections/News.tsx
+++ src/app/components/sections/News.tsx
@@ -1,73 +1,73 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-import React from 'react'
+import React from 'react';
 import { useTranslations } from 'next-intl';
-import {unstable_setRequestLocale} from 'next-intl/server';
 
 
-const callouts = [
+
+const posts = [
     {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
-      imageSrc: 'https://about.fb.com/wp-content/uploads/2024/02/Facebook-News-Update_US_AU_Header.jpg?w=1920',
-      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '/es/news',
-    },
-    {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+      id: 1,      
       href: '#',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzdQr1lkzMWyBSS3BW8e44omV7OtyhbPDa_A&s',
     },
     {
-      name: 'Travel',
-      description: 'Daily commute essentials',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-      href: '#',
+        id: 2,      
+        href: '#',
+        imageUrl:
+            'https://www.fundacionkonex.org/custom/web/data/imagenes/repositorio/2010/6/1/818/2016031505482167d16d00201083a2b118dd5128dd6f59.jpg',
     },
+    {
+        id: 3,      
+        href: '#',
+        imageUrl:
+            'https://otslist.boletinoficial.gob.ar/static/app/css/logo-bora.png',
+    },
+    // More posts...
   ]
   
   export default function NewsSection() {
+    const t = useTranslations('NewsSection');
     return (
-      <div className="bg-gray-100">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
-  
-            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-              {callouts.map((callout) => (
-                <div key={callout.name} className="group relative">
-                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                    <img
-                      alt={callout.imageAlt}
-                      src={callout.imageSrc}
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-                  <h3 className="mt-6 text-sm text-gray-500">
-                    <a href={callout.href}>
+      <div className="bg-white py-24 sm:py-20">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('header')} </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              {t('header1')}
+            </p>
+          </div>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post) => (
+              <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+                <div className="flex items-center gap-x-4 text-xs">
+                  <p className="text-gray-500">
+                    {t('date1')}
+                  </p>
+                  <p className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                    {t('category1')}
+                  </p>
+                </div>
+                <div className="group relative">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href={post.href}>
                       <span className="absolute inset-0" />
-                      {callout.name}
+                      {t('title1')}
                     </a>
                   </h3>
-                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{t('description1')}</p>
                 </div>
-              ))}
-            </div>
+                <div className="relative mt-8 flex items-center gap-x-4">
+                  <img alt="" src={post.imageUrl} className="h-10 w-10 rounded-full bg-gray-50" />
+                  <div className="text-sm leading-6">
+                    <p className="font-semibold text-gray-900">                      
+                        <span className="absolute inset-0" />
+                        {t('author1')}                      
+                    </p>
+                    <p className="text-gray-600">{t('media1')}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
           </div>
         </div>
       </div>
