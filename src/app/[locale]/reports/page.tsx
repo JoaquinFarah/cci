c8b33282ef0042caf7e05a4ec32d80164f0239f2
JFarah <joaquinefarah@gmail.com>
2024-08-28 12:45:16 -0300

    CSS and format add

--- src/app/[locale]/reports/page.tsx
+++ src/app/[locale]/reports/page.tsx
@@ -11,72 +11,45 @@ const ReportsPage: React.FC = () => {
         <h2 className="text-3xl font-bold text-start text-gray-800 mb-8 mt-16">
           {t('header')}
         </h2>
-        <div className="grid grid-rows-1 md:grid-rows-2 lg:grid-rows-4 gap-2">          
-            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">
-              {/* <div className="flex justify-center mb-4">
-                <Image
-                  src={"/"}
-                  alt={"img1"}
-                  width={60}
-                  height={60}
-                />
-              </div> */}
+        <div className="grid grid-rows-1 md:grid-rows-2 lg:grid-rows-4 gap-2">
+                    
+            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">             
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title1')}
               </h3>
-              <p className="text-gray-600">
+              <h1 className="text-gray-500 text-xs">12/08/2024 | Economia</h1>
+              <p className="text-gray-700">
                 {t('report1')}
               </p>
             </div>   
 
-            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">
-              {/* <div className="flex justify-center mb-4">
-                <Image
-                  src={"/"}
-                  alt={"img2"}
-                  width={60}
-                  height={60}
-                />
-              </div> */}
+            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title2')}
               </h3>
-              <p className="text-gray-600">
+              <h1 className="text-gray-500 text-xs">02/07/2024 | Marketing</h1>
+              <p className="text-gray-700">
                 {t('report2')}
               </p>
             </div>  
 
-            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">
-              {/* <div className="flex justify-center mb-4">
-                <Image
-                  src={"/"}
-                  alt={"img3"}
-                  width={60}
-                  height={60}
-                />
-              </div> */}
+            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title3')}
               </h3>
-              <p className="text-gray-600">
+              <h1 className="text-gray-500 text-xs">12/04/2024 | Economia</h1>
+              <p className="text-gray-700">
                 {t('report3')}
               </p>
             </div>    
 
-            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">
-              {/* <div className="flex justify-center mb-4">
-                <Image
-                  src={"/"}
-                  alt={"img4"}
-                  width={60}
-                  height={60}
-                />
-              </div> */}
+            <div className="bg-white p-6 rounded-lg text-start hover:shadow-2xl transition-all">             
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                {t('title4')}
+                TITULO DEL PROXIMO REPORTE
               </h3>
-              <p className="text-gray-600">
-                {t('text4')}
+              <h1 className="text-gray-500 text-xs">Fecha | Marketing</h1>
+              <p className="text-gray-700">
+                TEXTO DEL PROXIMO REPORTE
               </p>
             </div>  
 
