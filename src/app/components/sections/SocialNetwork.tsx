a68aeb8bd01aac559bb0282ec926e025d880b8a8
JFarah <joaquinefarah@gmail.com>
2024-08-30 10:35:50 -0300

    SocialNetworks fix and image add

--- src/app/components/sections/SocialNetwork.tsx
+++ src/app/components/sections/SocialNetwork.tsx
@@ -1,53 +1,46 @@
 import React from 'react';
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import Link from 'next/link';
+import Image from 'next/image';
 
-
-// const SocialNetworkSection: React.FC = () => {
-//   return (
-//     <div className="flex space-x-4">
-
-//     <Link href="https:www.ypf.com" passHref>
-//         <div className="flex items-center space-x-2 text-blue-500 hover:text-blue-700 transition-colors">
-//             <FaFacebookF size={20} />
-//             <span>Home</span>
-//         </div>
-//     </Link>
-     
-//     </div>
-//   );
-// };
-
-// export default SocialNetworkSection;
 
 
 export default function SocialNetworkSection() {
     return (
       <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-            Trusted by the world’s most innovative teams
+        <Image
+              src={"/socialnetworks1.jpg"}
+              alt={"social"}
+              // layout="fill"
+              // objectFit="cover"
+              width={1000}
+              height={1000}
+              className="h-52 w-full"
+            />
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">        
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">          
+            Social Networks<br/>Seguinos en nuestras redes
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            <Link href="https://www.facebook.com/ypfoficial" passHref>
-              <a className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-blue-600 transition-colors lg:col-span-1">
+            <Link href="https://www.facebook.com/ypfoficial" >
+              <div className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-blue-600 transition-colors lg:col-span-1">
                 <FaFacebookF size={48} />
-              </a>
+              </div>
             </Link>
-            <Link href="https://twitter.com/YPFoficial" passHref>
-              <a className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-blue-400 transition-colors lg:col-span-1">
+            <Link href="https://twitter.com/YPFoficial" >
+              <div className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-blue-400 transition-colors lg:col-span-1">
                 <FaTwitter size={48} />
-              </a>
+              </div>
             </Link>
-            <Link href="https://www.instagram.com/ypfoficial/" passHref>
-              <a className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-pink-500 transition-colors lg:col-span-1">
+            <Link href="https://www.instagram.com/ypfoficial/" >
+              <div className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-pink-500 transition-colors lg:col-span-1">
                 <FaInstagram size={48} />
-              </a>
+              </div>
             </Link>
-            <Link href="https://www.linkedin.com/company/ypf" passHref>
-              <a className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-blue-700 transition-colors sm:col-start-2 lg:col-span-1">
+            <Link href="https://www.linkedin.com/company/ypf" >
+              <div className="col-span-2 max-h-12 w-full flex justify-center items-center text-gray-900 hover:text-blue-700 transition-colors sm:col-start-2 lg:col-span-1">
                 <FaLinkedinIn size={48} />
-              </a>
+              </div>
             </Link>
             {/* Puedes agregar más íconos si lo deseas */}
           </div>
