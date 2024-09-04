e4544fa5e7519decfea1cf58dd5dbd5448ef46dd
JFarah <joaquinefarah@gmail.com>
2024-09-04 17:44:57 -0300

    minor fixes

--- src/app/[locale]/partners/page.tsx
+++ src/app/[locale]/partners/page.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
-import { count, countReset } from 'console';
+
 
 const data = [
     {
