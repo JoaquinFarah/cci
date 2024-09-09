7cef4b30e3770acc88ba4d85032fd6de966ba706
JFarah <joaquinefarah@gmail.com>
2024-09-09 19:53:45 -0300

    Link add to servicesection

--- src/app/components/sections/Service.tsx
+++ src/app/components/sections/Service.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 
 const ServiceSection: React.FC = () => {
@@ -11,42 +12,42 @@ const ServiceSection: React.FC = () => {
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">{t('header')}</h2>
         <div className="min-h-0.5 bg-gray-200 mb-8"></div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">                    
-            
-            <div className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
+          
+            <Link href="/es/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title1')}
               </h3>
               <p className="text-gray-600">
                 {t('text1')}
               </p>
-            </div>   
+            </Link>   
 
-            <div className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
+            <Link href="/es/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title2')}
               </h3>
               <p className="text-gray-600">
                 {t('text2')}
               </p>
-            </div>  
+            </Link>  
 
-            <div className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
+            <Link href="/es/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title3')}
               </h3>
               <p className="text-gray-600">
                 {t('text3')}
               </p>
-            </div>    
+            </Link>    
 
-            <div className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">             
+            <Link href="/es/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">             
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
                 {t('title4')}
               </h3>
               <p className="text-gray-600">
                 {t('text4')}
               </p>
-            </div>  
+            </Link>  
 
         </div>
       </div>
