a38f4c3c74c970998cb781533b2e77f750d41c0e
JFarah <joaquinefarah@gmail.com>
2024-09-09 20:25:23 -0300

    Button add at newssection

--- src/app/components/sections/News.tsx
+++ src/app/components/sections/News.tsx
@@ -70,6 +70,11 @@ const posts = [
               </article>
             ))}
           </div>
+          <div className="flex justify-center mt-12">
+            <button type="button" className="text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2">            
+              Asociate a nuestro NewsLetter
+            </button>
+          </div>
         </div>
       </div>
     )
