f50889847f4ff68da0478cb1bb69dd137fa50200
JFarah <joaquinefarah@gmail.com>
2024-10-02 13:43:20 -0300

    Newsletter button

--- src/app/components/sections/News.tsx
+++ src/app/components/sections/News.tsx
@@ -1,5 +1,31 @@
+"use client"
 import React from 'react';
 import { useTranslations } from 'next-intl';
+import Swal from 'sweetalert2';
+
+const askMail = () =>{
+  Swal.fire({
+    title: 'Newsletter',
+    text: 'Ingrese su mail para asociarse',    
+    input: 'email',
+    inputPlaceholder: "ejemplo@mail.com",
+    showCancelButton: true,
+    cancelButtonColor: "#d33",
+    cancelButtonText: 'Cancelar',
+    confirmButtonText: 'Aceptar',    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "¡Bienvenido!",
+        text: "Gracias por subscribirte al newsletter",
+        icon: "success"
+      });
+    }
+  });
+};
+
+
+
 
 
 const posts = [
@@ -70,8 +96,9 @@ const posts = [
             ))}
           </div>
           <div className="flex justify-center mt-12">
-            <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">{t('button')}</button>
+            <button type="button" onClick={askMail} className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">{t('button')}</button>
           </div>
+          
         </div>
       </div>
     ) 
