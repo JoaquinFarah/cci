a9fe6ff984bd4015d78c6728aed081009740ef83
JFarah <joaquinefarah@gmail.com>
2024-10-03 10:37:50 -0300

    Images changes, news add

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import { useTranslations } from 'next-intl';
+
 
 const callouts = [
     {
@@ -24,6 +23,7 @@ const callouts = [
       imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNQWAy4Tmruo_iSKJpZ2ctHgj7vLFSsx4a8w&s',
       imageAlt: 'Mendoza news',
       href: '#',
+      // usar la pagina del blog como plantilla, ver si es viable
     },
     {
       name: 'Tragedia en el Mar',
@@ -31,14 +31,7 @@ const callouts = [
       imageSrc: 'https://estaticos-cdn.prensaiberica.es/clip/2a329234-77e4-46be-b947-7faed8a2ae08_16-9-discover-aspect-ratio_default_0.jpg',
       imageAlt: 'World news',
       href: 'https://www.elperiodico.com/es/internacional/20240820/italia-busqueda-multimillonario-britanico-mike-lynch-naufragio-sicilia-107172497',
-    },
-    {
-      name: 'Mendoza',
-      description: 'Cambios para los comercios de Mendoza',
-      imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNQWAy4Tmruo_iSKJpZ2ctHgj7vLFSsx4a8w&s',
-      imageAlt: 'Mendoza news',
-      href: '#',
-    },
+    },    
     {
       name: 'Global news',
       description: 'El las relaciones internacionales en Europa',
@@ -46,6 +39,90 @@ const callouts = [
       imageAlt: 'World news',
       href: '#',
     },
+    {
+      name: 'Ferias',
+      description: 'CCI invita a productores frutihortícolas y exportadores a participar online en la Macfrut 2024 de Italia',
+      imageSrc: 'https://enolife.com.ar/es/wp-content/uploads/2024/04/Macfrut-2024-1024x440.jpg',
+      imageAlt: 'Expo',
+      href: 'https://enolife.com.ar/es/cci-invita-a-productores-frutihorticolas-y-exportadores-a-participar-online-en-la-macfrut-2024-de-italia/',
+    },
+    {
+      name: 'Ferias',
+      description: 'Simei 2024: destilados, cerveza y aceite de oliva tendrán su lugar junto al vino y toda la tecnología',
+      imageSrc: 'https://enolife.com.ar/es/wp-content/uploads/2024/06/Imagen2-10-1024x440.jpg',
+      imageAlt: 'simei',
+      href: 'https://enolife.com.ar/es/simei-2024-destilados-cerveza-y-aceite-de-oliva-tendran-su-lugar-junto-al-vino-y-toda-la-tecnologia/',
+    },
+    {
+      name: 'Ferias',
+      description: 'La Cámara de Comercio Italiana en Mendoza ofrece beneficios para viajar a la Feria Simei Milán',
+      imageSrc: 'https://enolife.com.ar/es/wp-content/uploads/2024/06/immagine_elenco_simei_2022_dettagli_1600px-1024x440.jpg',
+      imageAlt: 'embotelladora',
+      href: 'https://enolife.com.ar/es/la-camara-de-comercio-italiana-en-mendoza-ofrece-beneficios-para-viajar-a-la-feria-simei-milan/',
+    },
+    {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    }, {
+      name: '',
+      description: '',
+      imageSrc: '',
+      imageAlt: '',
+      href: '',
+    },
+
+    
+    
+    
+    
+    
+    
+    // {
+    //   name: '',
+    //   description: '',
+    //   imageSrc: '',
+    //   imageAlt: '',
+    //   href: '',
+    // },
     
    
     
@@ -58,24 +135,24 @@ const callouts = [
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('title')} </h2>
-            <div className="min-h-0.5 bg-gray-200 mb-4 mt-4"></div>
-            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            <div className="min-h-0.5 bg-gray-200 mb-4"></div>
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-2 lg:space-y-0">
               {callouts.map((callout) => (
-                <div key={callout.name} className="group relative">
-                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                <div key={callout.name} className="group relative rounded-lg px-1 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
+                  <div className="relative mt-5 h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                     <img
                       alt={callout.imageAlt}
                       src={callout.imageSrc}
                       className="h-full w-full object-cover object-center"
                     />
                   </div>
-                  <h3 className="mt-6 text-sm text-gray-500">
+                  <h3 className="mt-4 text-sm text-gray-500 px-4">
                     <a href={callout.href}>
                       <span className="absolute inset-0" />
                       {callout.name}
                     </a>
                   </h3>
-                  <p className="text-base font-semibold text-gray-900 mb-6">{callout.description}</p>
+                  <p className="text-base font-semibold text-gray-900 mb-6 text-left px-4">{callout.description}</p>
                 </div>
               ))}
             </div>
