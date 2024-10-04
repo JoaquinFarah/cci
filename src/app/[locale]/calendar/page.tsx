8aae13506b9b5aae03d485c6014962a6fa044f84
JFarah <joaquinefarah@gmail.com>
2024-10-04 13:26:58 -0300

    Calendar pop up add

--- src/app/[locale]/calendar/page.tsx
+++ src/app/[locale]/calendar/page.tsx
@@ -6,113 +6,100 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import multiMonthPlugin from '@fullcalendar/multimonth';
 import dayjs from 'dayjs';
-
-
+import Swal from 'sweetalert2';
 
 export default function Calendar() {
-  
-    const events = [    
-    { title: 'TEST Event 1', 
-        start: dayjs('2024-8-12T12:00:00').toDate(),
-        end: dayjs('2024-8-14T12:00:00').toDate(),
-        color: 'red',
-        url: 'https://www.linkedin.com' },
-
-    { title: 'FERIA AMERICANA', 
-        start: dayjs('2024-9-12T12:00:00').toDate(),
-        end: dayjs('2024-9-14T12:00:00').toDate(),
-        color: 'green' },
+  const events = [   
+    { 
+      title: 'SIMEI - Fiera Milano', 
+      start: dayjs('2024-12-12T8:00:00').toDate(),
+      end: dayjs('2024-12-15T12:00:00').toDate(),
+      color: 'orange',
+      description: 'Nos encontramos en la recta final para sumarse a la misión VIP de la Cámara de Comercio Italiana de Mendoza a la feria SIMEI 2024, que se llevará a cabo en Fiera Milano del 12 al 15 de noviembre. Este es el evento más importante del mundo para el sector enológico, y como parte de nuestra misión VIP, tendrán acceso exclusivo a una serie de beneficios únicos.<br/><br/>Eventos y Temas Destacados en SIMEI 2024:<br/>1. Vino: Técnica, Sostenibilidad y Nuevos Estilos<br/>- Estabilización tartárica, desalcoholización, crianza en madera y cerámica.<br/>- Influencia del cambio climático, sostenibilidad y legislación sobre ingredientes del vino.<br/>2. BeerForum: Oportunidad de Actualización para el Sector Cervecero<br/>- Producción y caracterización de cereales, malta, lúpulo.<br/>- Valorización de aromas, selección de levaduras, cervezas IGA, uso de subproductos.<br/>3. Distillo: Futuros y Tendencias en el Mundo de los Spirits<br/>- Análisis del mercado de spirits y destilados, estudios de caso reales.<br/>- Futuro del brandy italiano, vermut, whisky y Calvados.<br/>4. Aceite de Oliva: Técnica, Marketing, Diseño y Economía<br/>- Encuentros organizados por Olio Officina sobre Mosh y Moah, aceites para conservas, aceites aromatizados.<br/>- Clasificación mercadotécnica, envases y diseño, calidad sensorial y etiquetado.<br/>Con un programa tan completo y variado, SIMEI 2024 promete ser una experiencia transformadora y enriquecedora para todos los asistentes.<br/>Para más información o para confirmar su participación, por favor contacte a nuestro equipo lo antes posible.'
+    },
+    { 
+      title: 'MacFrut 2024', 
+      start: dayjs('2024-05-08T10:00:00').toDate(),
+      end: dayjs('2024-05-10T02:00:00').toDate(),
+      color: 'green',
+      description: 'del 08 al 10 de Mayo',
+      footer: '<a href="/">Link a la noticia</a>' 
+           
+    },
     
-    { title: 'Feria Educativa Italiana evento dependiente del consulado en Mendoza', 
-        start: dayjs('2024-1-02T8:00:00').toDate(),
-        end: dayjs('2024-1-03T12:00:00').toDate(),
-        color: 'orange' },
-
-    { title: 'TEST Event 4', 
-        start: dayjs('2024-10-17T10:00:00').toDate(),
-        end: dayjs('2024-10-17T02:00:00').toDate(),
-        color: 'purple'        
+    { title: 'Sitevinitech', 
+        start: dayjs('2024-05-15T12:00:00').toDate(),
+        end: dayjs('2024-05-17T12:00:00').toDate(),
+        color: 'black' 
     },
-
-    { title: 'TEST Event 5', 
-        start: dayjs('2024-4-12T12:00:00').toDate(),
-        end: dayjs('2024-4-13T12:00:00').toDate(),
-        color: 'yellow' 
-    },
-
-    { title: 'TEST Event 6', 
-        start: dayjs('2024-3-12T12:00:00').toDate(),
-        end: dayjs('2024-3-14T12:00:00').toDate(),
+    { title: 'Expo San Juan Mineria', 
+        start: dayjs('2024-05-21T12:00:00').toDate(),
+        end: dayjs('2024-05-23T12:00:00').toDate(),
         color: 'blue' 
     },
-
-    { title: 'TEST Event 8', 
-        start: dayjs('2024-9-02T06:00:00').toDate(),
-        end: dayjs('2024-9-02T10:00:00').toDate(),
-        color: 'red' 
+    { title: 'Jornadas de Ciencias Económicas', 
+        start: dayjs('2024-08-28T06:00:00').toDate(),
+        end: dayjs('2024-08-30T10:00:00').toDate(),
+        color: 'purple' 
     },
-
-    { title: 'TEST Event 9', 
-        start: dayjs('2024-9-02T06:00:00').toDate(),
-        end: dayjs('2024-9-02T15:00:00').toDate(),
+    { title: 'Economía Para Todos', 
+        start: dayjs('2024-09-21T06:00:00').toDate(),
+        end: dayjs('2024-09-21T15:00:00').toDate(),
         color: 'blue',
-        url:'https://www.linkedin.com' 
+         
     },
-
-    { title: 'TEST Event 10', 
-        start: dayjs('2024-7-02T06:00:00').toDate(),
-        end: dayjs('2024-7-04T15:00:00').toDate(),
+    { title: 'Economía Para Todos', 
+        start: dayjs('2024-10-12T06:00:00').toDate(),
+        end: dayjs('2024-10-12T15:00:00').toDate(),
         color: 'blue',
-        url:'https://www.linkedin.com' 
+         
     },
+    { title: 'Capacitaciones Ospitalità', 
+        start: dayjs('2024-10-17T18:00:00').toDate(),
+        end: dayjs('2024-10-17T22:00:00').toDate(),
+        color: 'red',
+        description: '',
+        footer: 'Puede inscribirse a la misma hacien click<a href="https://forms.gle/DfeaZ3onYD1RVZ3N6"> <u>AQUI</u></a>'
+         
+    },
+    
+  ];
 
-       
-];
+  const handleEventClick = (clickInfo) => {
+    Swal.fire({
+      title: clickInfo.event.title,
+      html: clickInfo.event.extendedProps.description,
+      confirmButtonText: 'Cerrar',
+      confirmButtonColor: 'red',
+      width: '80%',
+      backdrop: true,
+      footer: clickInfo.event.extendedProps.footer || '',
+      
+    });
+  };
 
   return (
     <div className="">
-        <h1 className="text-2xl font-bold">Calendar</h1>
-        <hr className="mt-10"/>
-        <div className="">
-
+      <h1 className="text-2xl font-bold">Calendar</h1>
+      <hr className="mt-10"/>
+      <div className="">
         <FullCalendar                
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, multiMonthPlugin]}
-            initialView={"multiMonthYear"}
-            headerToolbar={{
-                left: 'prev,next, today',
-                center: 'title',
-                right: 'dayGridMonth, timeGridWeek, timeGridDay, multiMonthYear'
-            }}
-            height={'100vh'}
-            events={events}
-            eventDisplay='block'   
-                    
-        />
-        </div>
-        <p className="mt-10">Espacio para poner descripciones de los eventos, imagenes o mas info
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            Espacio
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            Espacio
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            Espacio
-        </p>
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, multiMonthPlugin]}
+          initialView={"multiMonthYear"}
+          headerToolbar={{
+            left: 'prev,next, today',
+            center: 'title',
+            right: 'dayGridMonth, timeGridWeek, timeGridDay, multiMonthYear'
+          }}
+          height={'100vh'}
+          events={events}
+          eventDisplay='block'
+          eventClick={handleEventClick}
+        />        
+      </div>
+      <p className="mt-10">Espacio para poner descripciones de los eventos, imagenes o mas info
+        
+      </p>
     </div>
   )
 }
-
