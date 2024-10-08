d8ffaaa462664ddf5da2c91f80f5ad4ba33d759b
JFarah <joaquinefarah@gmail.com>
2024-10-08 14:42:23 -0300

    links add

--- src/app/[locale]/calendar/page.tsx
+++ src/app/[locale]/calendar/page.tsx
@@ -124,38 +124,45 @@ const events = [
     start: dayjs('2024-12-12T8:00:00').toDate(),
     end: dayjs('2024-12-15T12:00:00').toDate(),
     color: 'orange',
-    description: 'Nos encontramos en la recta final para sumarse a la misión VIP de la Cámara de Comercio Italiana de Mendoza a la feria SIMEI 2024, que se llevará a cabo en Fiera Milano del 12 al 15 de noviembre. Este es el evento más importante del mundo para el sector enológico, y como parte de nuestra misión VIP, tendrán acceso exclusivo a una serie de beneficios únicos.<br/><br/>Eventos y Temas Destacados en SIMEI 2024:<br/>1. Vino: Técnica, Sostenibilidad y Nuevos Estilos<br/>- Estabilización tartárica, desalcoholización, crianza en madera y cerámica.<br/>- Influencia del cambio climático, sostenibilidad y legislación sobre ingredientes del vino.<br/>2. BeerForum: Oportunidad de Actualización para el Sector Cervecero<br/>- Producción y caracterización de cereales, malta, lúpulo.<br/>- Valorización de aromas, selección de levaduras, cervezas IGA, uso de subproductos.<br/>3. Distillo: Futuros y Tendencias en el Mundo de los Spirits<br/>- Análisis del mercado de spirits y destilados, estudios de caso reales.<br/>- Futuro del brandy italiano, vermut, whisky y Calvados.<br/>4. Aceite de Oliva: Técnica, Marketing, Diseño y Economía<br/>- Encuentros organizados por Olio Officina sobre Mosh y Moah, aceites para conservas, aceites aromatizados.<br/>- Clasificación mercadotécnica, envases y diseño, calidad sensorial y etiquetado.<br/><br/>Con un programa tan completo y variado, SIMEI 2024 promete ser una experiencia transformadora y enriquecedora para todos los asistentes.<br/>Para más información o para confirmar su participación, por favor contacte a nuestro equipo lo antes posible.'
+    description: 'Nos encontramos en la recta final para sumarse a la misión VIP de la Cámara de Comercio Italiana de Mendoza a la feria SIMEI 2024, que se llevará a cabo en Fiera Milano del 12 al 15 de noviembre. Este es el evento más importante del mundo para el sector enológico, y como parte de nuestra misión VIP, tendrán acceso exclusivo a una serie de beneficios únicos.<br/><br/>Eventos y Temas Destacados en SIMEI 2024:<br/>1. Vino: Técnica, Sostenibilidad y Nuevos Estilos<br/>- Estabilización tartárica, desalcoholización, crianza en madera y cerámica.<br/>- Influencia del cambio climático, sostenibilidad y legislación sobre ingredientes del vino.<br/>2. BeerForum: Oportunidad de Actualización para el Sector Cervecero<br/>- Producción y caracterización de cereales, malta, lúpulo.<br/>- Valorización de aromas, selección de levaduras, cervezas IGA, uso de subproductos.<br/>3. Distillo: Futuros y Tendencias en el Mundo de los Spirits<br/>- Análisis del mercado de spirits y destilados, estudios de caso reales.<br/>- Futuro del brandy italiano, vermut, whisky y Calvados.<br/>4. Aceite de Oliva: Técnica, Marketing, Diseño y Economía<br/>- Encuentros organizados por Olio Officina sobre Mosh y Moah, aceites para conservas, aceites aromatizados.<br/>- Clasificación mercadotécnica, envases y diseño, calidad sensorial y etiquetado.<br/><br/>Con un programa tan completo y variado, SIMEI 2024 promete ser una experiencia transformadora y enriquecedora para todos los asistentes.<br/>Para más información o para confirmar su participación, por favor contacte a nuestro equipo lo antes posible.',
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>'
 },
 { 
     title: 'MacFrut 2024', 
     start: dayjs('2024-05-08T10:00:00').toDate(),
     end: dayjs('2024-05-10T02:00:00').toDate(),
-    color: 'green',               
+    color: 'green',
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>'               
 },    
 { title: 'Sitevinitech', 
     start: dayjs('2024-05-15T12:00:00').toDate(),
     end: dayjs('2024-05-17T12:00:00').toDate(),
-    color: 'black', 
+    color: 'black',
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>' 
 },
 { title: 'Expo San Juan Mineria', 
     start: dayjs('2024-05-21T12:00:00').toDate(),
     end: dayjs('2024-05-23T12:00:00').toDate(),
-    color: 'blue', 
+    color: 'blue',
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>' 
 },
 { title: 'Jornadas de Ciencias Económicas', 
     start: dayjs('2024-08-28T06:00:00').toDate(),
     end: dayjs('2024-08-30T10:00:00').toDate(),
-    color: 'purple', 
+    color: 'purple',
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>' 
 },
 { title: 'Economía Para Todos', 
     start: dayjs('2024-09-21T06:00:00').toDate(),
     end: dayjs('2024-09-21T15:00:00').toDate(),
-    color: 'blue',       
+    color: 'blue',  
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>'     
 },
 { title: 'Economía Para Todos', 
     start: dayjs('2024-10-12T06:00:00').toDate(),
     end: dayjs('2024-10-12T15:00:00').toDate(),
     color: 'blue',
+    footer:'<a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMgsFzKBWLbcbvlRgwnzTrFcQsPVBMcWwvSZFPGHLDckdPQZhJgqNHxjknLqFsbFMLrM" target="_blank" autofocus>Para participar del evento, haga click <u>AQUI</u></a>'
 },
 { title: 'Capacitaciones Ospitalità', 
     start: dayjs('2024-10-17T18:00:00').toDate(),
