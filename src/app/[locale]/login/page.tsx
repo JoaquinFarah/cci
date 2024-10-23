48745d1764b63dbf98b72e109a5af7a593d6c037
JFarah <joaquinefarah@gmail.com>
2024-10-23 10:54:04 -0300

    sign up update

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -72,7 +72,7 @@ export default async function LoginPage() {
               </button>           
 
               <form>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSc5MFqeKQvScQZpxmZ0ugIknLIPuRwpjd2YNSj1Y3VDpe37Bw/viewform" target="_blank" 
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFIbVYx0fyH8RVm_8cK07Dzt3qgTBY29vmQmM-WP4lcx_IWg/viewform" target="_blank" 
                   className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Registrarse
                 </a>
               </form>
