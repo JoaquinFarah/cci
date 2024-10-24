e77557af393a01514f73a6e1369de4a2433e7572
JFarah <joaquinefarah@gmail.com>
2024-10-24 15:31:41 -0300

    about page fixes

--- src/app/[locale]/servicepage/page.tsx
+++ src/app/[locale]/servicepage/page.tsx
@@ -1,44 +1,19 @@
 "use client"
-import React, {useEffect} from 'react'
+import React from 'react'
 import { useTranslations } from 'next-intl';
-import {unstable_setRequestLocale} from 'next-intl/server';
-import Swal from 'sweetalert2';
 import { ServicePopUp } from '@/app/components/sections/ServiceRequesPopUp';
+import Image from 'next/image';
 
 
-const askService = () => {
-  Swal.fire({
-    title: 'Solicitud de servicio',
-    html: `Escribanos a:
-    <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCHrjDvbDTQTTRwkgvfMPlVsfbkHssVDmZXXGGWnHmHqzmfNvGZXQFbHFXbgdkWWcGLWKGlq" target="_blank" autofocus><u>info@ccimendoza.com</u></a> </br> indicandonos su nombre, n° de socio y el servicio solicitado.</br>
-    </br>No es socio? <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFIbVYx0fyH8RVm_8cK07Dzt3qgTBY29vmQmM-WP4lcx_IWg/viewform" target="_blank" autofocus><u>Registrese</u></a>`,
-    icon: 'info',
-    confirmButtonText: 'Cerrar'
-    // closeButtonText: 'true'
-    // showDenyButton: 'true'
-    // denyButtonText: 'Registrarse', 
-    // denyButtonColor: '#32B723',
-    // denyButtonLink: '/es/login'
-    
-  })
-}
-
-
-export default function ServicePage({ params: { locale } }) {
+export default function ServicePage() {
   const t = useTranslations('ServicePage');
-
-  useEffect(() => {
-    // Lógica para manejar el locale en el lado del cliente si es necesario
-    console.log(`Locale actual: ${locale}`);
-  }, [locale]);
-
+ 
   return (
     <div className="flex h-full bg-white">
       <div className="flex flex-col items-start justify-start w-1/2 p-10 mt-20">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">{t('header')}</h1> 
         <div className="min-h-0.5 flex bg-gray-200 mb-4 mt-4"></div>                            
-        <p className="text-lg text-gray-700">        
-        
+        <div className="text-lg text-gray-700"> 
 
           <span className="font-bold uppercase">{t('title1')}</span><br />          
           <span className="font-semibold">{t('subtitle1a')}</span><br />          
@@ -93,9 +68,9 @@ export default function ServicePage({ params: { locale } }) {
           <ServicePopUp/>
           </span>      
           
-        </p>
+        </div>
       </div>
-      <img src="/hombresnegocio3.jpg" alt="salute" className="object-cover h-full w-1/2 mt-24 shadow-2xl" />
+      <Image src="/hombresnegocio3.jpg" alt="salute" width={1000} height={1000} className="object-cover h-full w-1/2 mt-24 shadow-2xl" />
     </div>    
   )
 }
