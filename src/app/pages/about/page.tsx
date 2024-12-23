cc45d12fb666de2b0681c94cb2ece6dc1eb20d9d
JFarah <joaquinefarah@gmail.com>
2024-12-23 18:09:45 -0300

    button add

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,4 +1,16 @@
+"use client"
+import Swal from 'sweetalert2';
 
+
+const popUp = () =>{
+  Swal.fire({
+    title: 'Contacto',
+    html: `
+    <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCHrjDvbDTQTTRwkgvfMPlVsfbkHssVDmZXXGGWnHmHqzmfNvGZXQFbHFXbgdkWWcGLWKGlq" target="_blank" autofocus><u>info@ccimendoza.com</u></a><br/> Andrade 30, Ciudad de Mendoza <br/>2617659988` ,
+    icon: 'info',
+    confirmButtonText: 'Cerrar'
+  })
+};
 
 const people = [
   {
@@ -169,7 +181,12 @@ export default function AboutPage() {
           </p>
           <p className="text-lg leading-8 text-gray-600 text-pretty">
           • Establecer contactos permanentes de colaboración con organismos e instituciones argentinos e italianos.
-          </p>                   
+          </p>       
+
+          <div className="flex justify-center mt-8">
+            <button type="button" onClick={popUp} className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Contactanos</button>
+          </div>
+
           
         </div>
         {/* <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-3 sm:gap-y-8 xl:col-span-1">
