9c265f427c7e3c90962e536a25c991b336db6c81
JFarah <joaquinefarah@gmail.com>
2024-12-23 20:28:57 -0300

    sponsor section hidden

--- src/app/components/sections/Sponsor.tsx
+++ src/app/components/sections/Sponsor.tsx
@@ -9,7 +9,7 @@ export default function SponsorSection() {
             {/* <br/>
             {t('description')} */}
           </h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          {/* <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <Image
               alt="Tekno"
               src="/tekno.png"
@@ -123,7 +123,7 @@ export default function SponsorSection() {
               className="col-span-2 max-h-36 w-full object-contain lg:col-span-1"
             />
 
-          </div>
+          </div> */}
         </div>
       </section>
     )
