7b2032537561111614e89be9d583a761f501ea6a
JFarah <joaquinefarah@gmail.com>
2024-12-23 15:56:52 -0300

    service section update

--- src/app/components/sections/Service.tsx
+++ src/app/components/sections/Service.tsx
@@ -13,7 +13,7 @@ const ServiceSection: React.FC = () => {
           
             <Link href="/pages/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-              Servicio Informático
+              Servicio Informativo
               </h3>
               <p className="text-gray-600">
               Nuestro servicio está diseñado para ayudar a las empresas a ingresar a nuevos mercados internacionales con información y análisis detallados, facilitando la toma de decisiones estratégicas.
@@ -22,28 +22,28 @@ const ServiceSection: React.FC = () => {
 
             <Link href="/pages/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-              Análisis de Mercado Personalizado
+              Eventos y Comunicación
               </h3>
               <p className="text-gray-600">
-              Ofrecemos un análisis detallado de mercados por país y sector, brindando a tu empresa una visión clara y precisa de las oportunidades y desafíos que se presentan en cada región.
+              Diseñamos experiencias únicas para conectar empresas a través de cenas de gala, eventos de networking y actividades exclusivas. Complementamos estos encuentros con estrategias de comunicación efectivas, que incluyen inserciones en medios, gestión de relaciones públicas y campañas adaptadas para impulsar el lanzamiento de empresas y productos en el mercado.
               </p>
             </Link>  
 
             <Link href="/pages/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">              
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-              Informes Normativos Específicos
+              Contactos Comerciales
               </h3>
               <p className="text-gray-600">
-              Proporcionamos dossiers informativos sobre regulaciones aduaneras, fiscales y beneficios disponibles, adaptados a las necesidades de tu negocio, para que puedas operar en nuevos mercados con seguridad y confianza.
+              Facilitamos la conexión de empresas con socios estratégicos, importadores, distribuidores y proveedores clave, adaptándonos a sus necesidades con o sin agenda de citas. Organizamos misiones comerciales, talleres con encuentros B2B y brindamos apoyo integral en la participación en ferias internacionales para potenciar su presencia en el mercado.
               </p>
             </Link>    
 
             <Link href="/pages/servicepage" className="bg-white p-6 hover:scale-105 rounded-lg text-center hover:shadow-2xl transition-all border-4 border-indigo-500/100">             
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-              Información sobre Ferias y Eventos
+              Asistencia y Consultoría
               </h3>
               <p className="text-gray-600">
-              Te mantenemos informado sobre las principales ferias y eventos de tu sector, permitiéndote conectar con las oportunidades más relevantes para el crecimiento de tu empresa.
+              Brindamos apoyo integral para el desarrollo y expansión de empresas, ofreciendo servicios que abarcan asistencia legal, fiscal y aduanera, creación de estructuras comerciales, estrategias de entrada al mercado y transferencia de tecnología. Además, facilitamos representación empresarial, promoción en nuestros canales y soporte operativo en cada etapa del proceso de establecimiento, adaptándonos a las necesidades específicas de su negocio.
               </p>
             </Link>  
 
